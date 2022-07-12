51d8d1065c9c9b9b2e427b44ad1b6000defd6b0e
Marcelo_Lima <marcelolima152021@gmail.com>
2022-07-12 15:48:52 -0300

    Cor do texto vermelho quando o numero for negativo

--- Assets/js/scripts.js
+++ Assets/js/scripts.js
@@ -2,17 +2,17 @@ var currentNumberWrapper = document.getElementById('currentNumber');
 var currentNumber = 0;
 
 function increment(){
-    if(currentNumber >= 10){
-        currentNumberWrapper.removeEventListener();
-    }
     currentNumber = currentNumber + 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber >= 0){
+        currentNumberWrapper.style.color = 'black';
+    }
 }
 
 function decrement(){
-    if(currentNumber <= 0 ){
-        currentNumberWrapper.removeEventListener();
-    }
     currentNumber = currentNumber - 1;
     currentNumberWrapper.innerHTML = currentNumber;
+    if(currentNumber < 0){
+        currentNumberWrapper.style.color = 'red';
+    }
 }
